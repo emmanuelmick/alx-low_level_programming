cb99d3c87a15d1c1015d2a039c3ab3ebe54df94f
Emmanuel Omena <emmanuel.omena33@outlook.com>
2022-06-10 12:19:33 -0700

    Added file 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - prints the alphabet in lowercase
+ * Return: Always 0
+ */
+int main(void)
+{
+for (char c = 'a'; c <= 'z'; c++)
+{
+	putchar(c);
+}
+putchar('\n');
+return (0);
+}
