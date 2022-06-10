3feeb57e8e26719d4314dd4e45417d02401d17ee
Emmanuel Omena <emmanuel.omena33@outlook.com>
2022-06-10 12:37:55 -0700

    Added file 3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints lowercase and uppercase characters
+ * Return: Always 0
+ */
+int main(void)
+{
+for (char c = 'a'; c <= 'z'; c++)
+{
+	putchar(c);
+}
+for (char j = 'A'; j <= 'Z'; j++)
+{
+	putchar(j);
+}
+putchar('\n');
+return (0);
+}
