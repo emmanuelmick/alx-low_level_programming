8b5170ffe957dfff22215bb052e1a0720e702412
Emmanuel Omena <emmanuel.omena33@outlook.com>
2022-06-10 13:02:52 -0700

    Added file 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints all base 10 numbers beginning from zero
+ * Return: Always 0
+ */
+int main(void)
+{
+int num;
+for (num = 0; num < 10; num++)
+{
+	printf("%d\n", num);
+}
+return (0);
+}
