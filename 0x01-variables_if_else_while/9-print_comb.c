cd8bbe43c40533dea471871ad9dca1d5d758b6f8
Emmanuel Omena <emmanuel.omena33@outlook.com>
2022-06-12 04:29:56 -0700

    Added fil 9-file_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible combinations of single-digit numbers.
+ * Return: Always 0
+ */
+int main(void)
+{
+int n;
+for (n = 48; n <= 57; n++)
+{
+	putchar(n);
+	if (n != 57)
+	{
+		putchar(",");
+	}
+}
+putchar("\n")
+return (0);
+}
