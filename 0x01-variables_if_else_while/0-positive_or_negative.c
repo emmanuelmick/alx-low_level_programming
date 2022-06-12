7276efb065e2e3104dea8c2c2c21f37a196cac3b
Emmanuel Omena <emmanuel.omena33@outlook.com>
2022-06-12 04:48:55 -0700

    Added file 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,7 +5,7 @@
 /*
  * main -> Assigns a random number to the variable n each time it is executed.
  * The result is then printed if the condition is met
- * Return : always 0
+ * Return: Always 0
  */
 int main(void)
 {
@@ -21,7 +21,7 @@ else if (n == 0)
 {
 	printf("%d is zero.", n);
 }
-else 
+else
 {
 	printf("%d is negative.", n);
 }
