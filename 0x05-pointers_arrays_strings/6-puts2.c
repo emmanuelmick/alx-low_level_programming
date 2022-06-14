7b7c681bbf79c54b1d27b1419c366446d83aaa74
Emmanuel Omena <emmanuel.omena33@outlook.com>
2022-06-14 14:28:29 -0700

    A dded puts2 function

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * puts2 - A function that prints any other characte of a string
+ * @str: parameter of the function
+ */
+void puts2(char *str)
+{
+	int i, len;
+
+	len = strlen(str);
+	for (i = 0; i < len; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
+
