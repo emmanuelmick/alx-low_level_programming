afbfd6e7176af6592c7e8312b31afa26ab6518ee
Emmanuel Omena <emmanuel.omena33@outlook.com>
2022-06-14 07:22:09 -0700

    looping through string characters

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - A function that prints the lenght of a given string
+ * @s: string pointer passed to the given function
+ * Return: the lenght of the string
+ */
+int _strlen(char *s)
+{
+	int lenght;
+
+	while (*s != '\0')
+	{
+		lenght += 1;
+		*s++
+	}
+	return (lenght)
+}
