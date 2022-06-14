f0e9205e70f8fae479fafef337f3bff59ebe9219
Emmanuel Omena <emmanuel.omena33@outlook.com>
2022-06-14 11:46:53 -0700

    Printing a string character

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _puts - A function that prints a string followed by a new line
+ * @str: The parameter declared in the function
+ */
+void _puts(char *str)
+{
+	for (; *str != '\0'; str++)
+	{
+		_putchar(*str);
+	}
+	putchar('\n');
+}
