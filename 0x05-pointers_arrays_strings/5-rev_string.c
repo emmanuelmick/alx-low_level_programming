81ba254851d2dcabf8c91761021fa1816f71a382
Emmanuel Omena <emmanuel.omena33@outlook.com>
2022-06-14 13:25:18 -0700

    Added a funcion that reverses an a string array

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * rev_string - A function which reverses an array of string
+ *@s; parameter used in the function
+ */
+void rev_string(char *s)
+{
+	char tmp;
+	int i, len1, len2;
+	len1 = 0;
+	len2 = 0;
+
+	while (s[len1] != '\0')
+		len1++;
+	len2 = len1;
+	for (i = 0; i < len1; i++)
+	{
+		tmp = s[i];
+		s[i] = s[len2];
+		s[len2] = tmp;
+		len2 -= 1;
+	}
+}
