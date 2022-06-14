e273e2a73ee202f3703f38e8e28b34c154147e9f
Emmanuel Omena <emmanuel.omena33@outlook.com>
2022-06-14 13:29:49 -0700

    Added to file 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -3,18 +3,19 @@
 
 /**
  * rev_string - A function which reverses an array of string
- *@s; parameter used in the function
+ *@s: parameter used in the function
  */
 void rev_string(char *s)
 {
 	char tmp;
 	int i, len1, len2;
+
 	len1 = 0;
 	len2 = 0;
 
 	while (s[len1] != '\0')
 		len1++;
-	len2 = len1;
+	len2 = len1 - 1;
 	for (i = 0; i < len1; i++)
 	{
 		tmp = s[i];
