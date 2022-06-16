585041b091fc31081cab7b6c01822d3660c2e836
Emmanuel Omena <emmanuel.omena33@outlook.com>
2022-06-16 02:29:30 -0700

    Added to file 1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ *_strncat - A function that concatenate two given string
+ * and append some character.
+ * @dest: parameter1
+ * @src: parameter2
+ * @n: parameter3
+ * Return: String
+ */
+int *_strncat(char *dest, char *src, int n)
+{
+	strncat(dest, src, n);
+
+	return (dest);
+}
