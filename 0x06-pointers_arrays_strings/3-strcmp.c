3eca8e8a314d12f3de53bcfcaa606f768fdbbcbc
Emmanuel Omena <emmanuel.omena33@outlook.com>
2022-06-19 08:46:04 -0700

    Strncmp function

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcmp - A function that compares two string
+ * @s1: parameter1
+ * @s2: parameter2
+ * Return: Integer
+ */
+int _strcmp(char *s1, char *s2)
+{
+	strcmp(s1, s2);
+	return (dest);
+}
