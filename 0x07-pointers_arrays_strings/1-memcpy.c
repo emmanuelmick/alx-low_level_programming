acf72dfecc3766f39a21da02042ebf89c4afae4a
Emmanuel Omena <emmanuel.omena33@outlook.com>
2022-06-21 01:28:17 -0700

    Added the _memcpy function

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -5,16 +5,13 @@
  * @dest: parameter 1
  * @src: parameter 2
  * @n: parameter 3
- * Return: A pointer
+ * Return: A string copied from source
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i = 0;
 
-	while (i < n)
-	{
-		*(dest +  i) = src;
-		i++;
-	}
+	for (i = 0; i < n; i++)
+		dest[i] = src[i]
 	return (dest);
 }
