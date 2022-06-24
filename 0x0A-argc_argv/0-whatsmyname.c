b75a1a76ff1eb404b155cb1a752da4eefc640379
Emmanuel Omena <emmanuel.omena33@outlook.com>
2022-06-24 11:03:37 -0700

    Modified file

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,14 +1,13 @@
 #include "main.h"
-#include <stdlib.h>
 #include <stdio.h>
 
 /**
- * _printname - A function that prints the name of a program and a new line
+ * main - A function that prints the name of a program and a new line
  * @argc: argument count
  * @argv: A string of characters
  * Return: Always 0
  */
-int _printname(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
