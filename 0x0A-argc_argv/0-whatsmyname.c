bfb8217bfc9d5cb0fb3e8f3f7839f392fe19bdaf
Emmanuel Omena <emmanuel.omena33@outlook.com>
2022-06-24 10:19:29 -0700

    Added _printname function

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * _argv - A function that prints the name of a program and a new line
+ * @argc - Argument count
+ * @argv - array of strings
+ * Return: The name of the program
+ */
+int _printname(int argc, char* argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("The name of the program is argv[%s]", argv[i]);
+	}
+	return (0);
+}
