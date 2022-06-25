051666911fbbbbc5bb5d0ce330f7d6c8d6e33ddf
Emmanuel Omena <emmanuel.omena33@outlook.com>
2022-06-25 10:37:19 -0700

    Hot fix

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,6 @@
 #include "main.h"
 #include <stdio.h>
-
+#include <stdlib.h>
 /**
  * main - A function that adds two numbers
  * @argc: Parameter1
@@ -9,13 +9,13 @@
  */
 int main(int argc, char *argv[])
 {
-	int result, num1, i, j, k;
+	int result, num, i, j, k;
 
 	if (argc == 1)
 		printf("0\n");
 	for (i = 0; i < argc; i++)
 	{
-		for (j = 0; argv[i][j] != '\0')
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
 			printf("%s\n", "Error");
 			return (1);
