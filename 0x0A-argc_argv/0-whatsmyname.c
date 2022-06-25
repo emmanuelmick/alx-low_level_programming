81480e14679aa2be07a265b6db8641fe5a55fc1b
Emmanuel Omena <emmanuel.omena33@outlook.com>
2022-06-25 08:31:35 -0700

    Added unused variable

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,7 +7,7 @@
  * @argv: A string of characters
  * Return: Always 0
  */
-int main(int argc, char *argv[])
+int main(int argc __attribute ((unused)), char *argv[])
 {
 	printf("%s\n", *argv);
 	return (0);
