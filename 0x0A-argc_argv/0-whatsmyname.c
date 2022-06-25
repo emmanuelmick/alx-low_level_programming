e92b2d44c4ef28595366cc0f213a8e993b528e1b
Emmanuel Omena <emmanuel.omena33@outlook.com>
2022-06-25 08:49:24 -0700

    Modified the 0-whatsmyname.c file

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,11 +3,11 @@
 
 /**
  * main - A function that prints the name of a program and a new line
- * @argc: argument count
- * @argv: A string of characters
+ * @argc: Argument count
+ * @argv: An array of command
  * Return: Always 0
  */
-int main(int argc __attribute ((unused)), char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", *argv);
 	return (0);
