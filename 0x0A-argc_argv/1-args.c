58bb83ec94e22e9ecd6b4f793a2803dfbca7433a
Emmanuel Omena <emmanuel.omena33@outlook.com>
2022-06-25 09:06:21 -0700

    Added file 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - A function that prints the number of argument received
+ * @argc: argument counter
+ * @argv: An array of string
+ * Return: Always 0
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
